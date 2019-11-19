3c965f83fdbafd2b11e59da755bf7190eb1c90bf
Gary Zhou <gary.zhou@berkeley.edu>
2019-11-19 01:23:29 -0800

    delete unneeded files

--- script.bash
+++
@@ -1,8 +0,0 @@
-#  py solver.py inputs/50.in outputs/ naive
-#  py output_validator.py inputs/50.in outputs/50.out naive
-
-#  py solver.py inputs/100.in outputs/ greedy
-#  py output_validator.py inputs/100.in outputs/100.out greedy
-
- py solver.py --all inputs/ outputs/ greedy
- py output_validator.py --all inputs/ outputs/ greedy
